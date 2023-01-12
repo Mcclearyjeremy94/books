603f0c4569ad4457c247bafa751fc4b0b8f5a6b3
Jeremy McCleary <110989624+Mcclearyjeremy94@users.noreply.github.com>
2023-01-12 13:38:03 -0800

    Add files via upload

---
+++ books.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SHOW TABLES
+-- Date: 2023-01-12 13:36
+*/
+INSERT INTO `` (`Tables_in_books`) VALUES ('authors');
+INSERT INTO `` (`Tables_in_books`) VALUES ('books');
+INSERT INTO `` (`Tables_in_books`) VALUES ('favorites');
+INSERT INTO `` (`Tables_in_books`) VALUES ('users');
